2db096d682563e78efc3d767547540e54fc571d8
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-05-09 12:14:45 +0930

    finished

---
+++ Farm.h
@@ -0,0 +1,23 @@
+#ifndef FARM_H
+#define FARM_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+// Farm class that is the bais for the project
+
+class Farm{
+     protected:
+        string name;
+        string location;
+        float balance;
+        wheatHarvastCount();
+         
+}
+;
+
+
+
+#endif

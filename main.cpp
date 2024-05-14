c29aeed7ccf0a01ea7146344770a1ee47a524234
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-05-14 12:46:45 +0930

    first image in to main

--- main.cpp
+++ main.cpp
@@ -7,7 +7,15 @@ int main()
     sf::RectangleShape rectangle(sf::Vector2f(200, 100)); // Size of the rectangle (width, height)
     rectangle.setFillColor(sf::Color::Blue); // Color of the rectangle
     rectangle.setPosition(400, 250); // Position of the rectangle (x, y)
+    sf::Texture TXcrops;
+
+    if (!TXcrops.loadFromFile("crops.png", sf::IntRect(100, 100, 320, 320))) {
+        // Handle error
+        return -1;
+    }
     // run the program as long as the window is open
+    sf::Sprite SPcrops(TXcrops);
+    SPcrops.setPosition(100, 100);
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
@@ -20,11 +28,12 @@ int main()
         }
 
         // clear the window with black color
-        window.clear(sf::Color(139, 69, 19));
+        window.clear(sf::Color(360, 360, 360));
     
         // draw everything here...
         // window.draw(...);
         window.draw(rectangle);
+        window.draw(SPcrops);
         // end the current frame
         window.display();
     }

aaf77bf1afeecfb54e78740fcfc1f3cce6cf5df8
Otto Hedger <a1891747@student.adelaide.edu.au>
2024-05-14 13:19:03 +0930

    partway through coding the main file to play the game

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
 int main()
 {
     // create the window
@@ -9,13 +9,13 @@ int main()
     rectangle.setPosition(400, 250); // Position of the rectangle (x, y)
     sf::Texture TXcrops;
 
-    if (!TXcrops.loadFromFile("crops.png", sf::IntRect(100, 100, 320, 320))) {
-        // Handle error
+    if (!TXcrops.loadFromFile("crops.png", sf::IntRect(0, 0, 550, 75))) {
+        std::cerr << "Failed to load crops.png" << std::endl;
         return -1;
     }
     // run the program as long as the window is open
     sf::Sprite SPcrops(TXcrops);
-    SPcrops.setPosition(100, 100);
+    SPcrops.setPosition(50, 10);
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
@@ -25,10 +25,14 @@ int main()
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::MouseButtonPressed){
+                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+                if ()
+            }
         }
 
         // clear the window with black color
-        window.clear(sf::Color(360, 360, 360));
+        window.clear(sf::Color(255, 255, 255));
     
         // draw everything here...
         // window.draw(...);
